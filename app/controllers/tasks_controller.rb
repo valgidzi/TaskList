c8a6e59d0ff344c1f1cf8c4964eae49c3b75d9ba
Valerie Gidzinski <valgidzi@gmail.com>
2018-09-17 16:18:11 -0700

    Tasks show action

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -5,7 +5,13 @@ TASKS = [
 ]
 
 class TasksController < ApplicationController
+
   def index
     @tasks = TASKS
+  end
+
+  def show
+    task_id = params[:id].to_i
+    @task = TASKS[task_id]
   end
 end
