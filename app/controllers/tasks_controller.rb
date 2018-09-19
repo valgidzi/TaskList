ffddf501cb0b03722999381ffc3feed2c1a717dc
Valerie Gidzinski <valgidzi@gmail.com>
2018-09-19 15:57:12 -0700

    Add form to create new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,4 +18,22 @@ class TasksController < ApplicationController
       head :not_found
     end
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(
+      name: params[:task][:name],
+      description: params[:task][:description],
+      completion_date: params[:task][:completion]
+    )
+
+    if @task.save
+      redirect_to tasks_path
+    else
+      render :new
+    end
+  end
 end
