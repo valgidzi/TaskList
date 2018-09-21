718a988e0a69ffdba1d723311348bff4a1499a54
Valerie Gidzinski <valgidzi@gmail.com>
2018-09-21 14:26:07 -0700

    Add link to task details

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
   post '/tasks', to: 'tasks#create'
   get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
 end
