878232e789645633cecbdecf3f84d73bb7e1e8dd
Valerie Gidzinski <valgidzi@gmail.com>
2018-09-28 16:06:34 -0700

    Refactor routes with resources method

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,25 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/tasks', to: 'tasks#index', as: 'tasks'
-  get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post '/tasks', to: 'tasks#create'
-
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
-
-  patch '/tasks/:id', to: 'tasks#update'
-  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  
+  resources :tasks
 
   get '/tasks/:id/mark_complete', to: 'tasks#mark_complete', as: 'mark_complete_task'
 
   get '/tasks/:id/unmark_complete', to: 'tasks#unmark_complete', as: 'unmark_complete_task'
 
-  delete '/tasks/:id', to: 'tasks#destroy'
-
-
 end
+
+  # get '/tasks', to: 'tasks#index', as: 'tasks'
+  # get '/tasks/new', to: 'tasks#new', as: 'new_task'
+  # post '/tasks', to: 'tasks#create'
+  #
+  # get '/tasks/:id', to: 'tasks#show', as: 'task'
+  #
+  # patch '/tasks/:id', to: 'tasks#update'
+  # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  #
+  # get '/tasks/:id/mark_complete', to: 'tasks#mark_complete', as: 'mark_complete_task'
+  #
+  # get '/tasks/:id/unmark_complete', to: 'tasks#unmark_complete', as: 'unmark_complete_task'
+  #
+  # delete '/tasks/:id', to: 'tasks#destroy'
